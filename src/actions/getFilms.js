bf19191f0a3dfbae62de9d83036f79431bd64c7f
shubich <shaa5@yahoo.com>
2017-10-17 12:51:07 +0300

    export api methods & easy change css

--- src/actions/getFilms.js
+++ src/actions/getFilms.js
@@ -4,15 +4,19 @@ import {
     GET_FILMS_FAILURE 
 } from '../constants/Films';
 
+import * as API from '../api';
+
 const getFilms = (query) => {
     return (dispatch) => {
         dispatch({
             type: GET_FILMS_REQUEST,
             query         
         })
-// https://api.themoviedb.org/3/search/movie?api_key=595f6d4c932627df7eb7d5c2f27a7e40&language=en-US&&query=taxi&page=1&include_adult=false
+        
+        const queryString = API.encodeQueryData({...API.QUERY, query});
+        let url = `${API.host}?${queryString}`;
 
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=595f6d4c932627df7eb7d5c2f27a7e40&language=en-US&&query=${query}&page=1&include_adult=false`)  
+        fetch(url)  
         .then(  
           function(response) {  
             if (response.status !== 200) {  
--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,7 @@ export const API_KEY = '595f6d4c932627df7eb7d5c2f27a7e40';
 export const host = 'https://api.themoviedb.org/3/search/movie';
 export const imagePrefix = 'https://image.tmdb.org/t/p/w640';
 
-// https://api.themoviedb.org/3/search/movie?api_key=595f6d4c932627df7eb7d5c2f27a7e40&language=en-US&&query=taxi&page=1&include_adult=false
-const encodeQueryData = (data) => {
+export const encodeQueryData = (data) => {
     let query = [];
     for (let item in data) {
         query.push(`${encodeURIComponent(item)}=${encodeURIComponent(data[item])}`);
@@ -11,28 +10,12 @@ const encodeQueryData = (data) => {
     return query.join('&');
 }
 
-const QUERY = {
+export const QUERY = {
     api_key: API_KEY,
     language: 'en-US',
-    include_adult: 'false'
+    include_adult: 'false',
+    page: '1'
 };
 
-// var data = { 'first name': 'George', 'last name': 'Jetson', 'age': 110 };
-// var querystring = encodeQueryData(data);
-
-// console.log(querystring);
-
-// export default {
-//     getFilms(params) {
-//         switch (params.searchFilter) {
-//             case 'title':
-//                 //let films = 
-//                 return '...';
-//             case 'director':
-//                 return '...';
-//         }
-//     },
-//     getFilmDetails(params) {
-//         // getFilmById
-//     }
-// }
+// request url exapmle
+// https://api.themoviedb.org/3/search/movie?api_key=595f6d4c932627df7eb7d5c2f27a7e40&language=en-US&&query=taxi&page=1&include_adult=false
