7d9d4ec171d3950cb3a177b7d5652c4b36d08ff5
shubich <shaa5@yahoo.com>
2017-10-24 12:44:57 +0300

    one more test

---
+++ src/components/__test__/PersonDetails.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { PersonDetails } from '../PersonDetails';
+import { shallow } from 'enzyme';
+
+let props;
+
+describe('PersonDetails', () => {
+    beforeEach(() => {
+        props = {
+            details: {
+                name: 'Pulp Fiction',
+                profile_path: '/img1234_0987',
+                popularity: '1999',
+                birthday: '8.3',
+                place_of_birth: ['Thriller', 'Crime'],
+                deathday: '154',
+                biography: 'Lorem ipsum dolor sit amet.'
+            },
+            getPersonDetails: jest.fn(),
+            getFilmsWithCast: jest.fn(),
+            setSearchType: jest.fn()
+        };
+    });
+
+    it('renders correctly with details', () => {
+        const wrapper = shallow(
+            <PersonDetails {...props} />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('renders correctly with fetching', () => {
+        const wrapper = shallow(
+            <PersonDetails {...props} fetching={true} />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('renders correctly without details', () => {
+        const wrapper = shallow(
+            <PersonDetails {...props} details={[]} />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+})
